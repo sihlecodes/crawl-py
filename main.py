eb5ec5e544b5eb9e1fc6ada4663e7d20bcd0ebc3
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-03-08 19:29:59 +0200

    Completed the working parts
    Added verbose and dry options
    Fixed bugs with include and exclude code

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import argparse
 import re
-import glob
+import shutil
 import os
 
 parser = argparse.ArgumentParser(description="Recursively moves files in subdirectories to the root path. Moves all files if --include or --exclude are not supplied.")
 parser.add_argument("path", metavar="PATH", nargs="?", default=".", help="The root path where crawling should begin")
+parser.add_argument("-v", "--verbose", action="store_true", help="Logs extra information")
+parser.add_argument("-d", "--dry", action="store_true", help="Only prints move operations. Does not move files.")
 
 group = parser.add_mutually_exclusive_group()
 
-group.add_argument("--include", type=re.compile, help="Takes a regex pattern, only files matching the pattern will be targeted for retrieval. Matching is done on both files and directories.")
-group.add_argument("--exclude", type=re.compile, help="Takes a regex pattern, only files not matching the pattern will be targeted for retrieval. Matching is done on both files and directories.")
+group.add_argument("-i", "--include", type=re.compile, help="Takes a regex pattern, only files matching the pattern will be targeted for retrieval. Matching is done on both files and directories.")
+group.add_argument("-x", "--exclude", type=re.compile, help="Takes a regex pattern, only files not matching the pattern will be targeted for retrieval. Matching is done on both files and directories.")
 
-def crawl(parent, args):    
+def crawl(parent, args):
+    print(parent)
     if not os.path.exists(parent):
         print(f"{parent}: folder does not exist.")
         return
@@ -25,13 +28,15 @@ def crawl(parent, args):
     for child in children:
         child = os.path.join(parent, child)
 
-        if args.exclude and args.exclude.search(child):
+        is_dir = os.path.isdir(child)
+
+        if not is_dir and args.exclude and args.exclude.search(child):
             if args.verbose:
                 print(f"{child}: file excluded by pattern")
                 
             continue
         
-        elif args.include:
+        elif not is_dir and args.include:
             if args.include.search(child):
                 if args.verbose:
                     print(f"{child}: file included by pattern")
@@ -42,17 +47,23 @@ def crawl(parent, args):
             crawl(child, args)
         else:
             # file moving behavior
-            print(f"preparing to move {child}")
+            fname = os.path.split(child)[-1]
+            new_path = os.path.join(args.path, fname)
+            old_path = child
+
+            if old_path != new_path:
+                if args.dry:
+                    print("mv", old_path, new_path)
+                else:
+                    shutil.move(old_path, new_path)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     
-    print("Crawl in progress... DO NOT CANCEL")
-    
-    if args.include:
-        print(args.include)
-        
-    elif args.exclude:
-        print(args.exclude)
+    if args.verbose:
+        print("Crawl in progress... DO NOT CANCEL")
     
     crawl(args.path, args)
+
+    if args.verbose:
+        print("Done")
